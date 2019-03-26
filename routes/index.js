61827870c2a252270fb0bf8e1dd8a4b7bbd2dd7e
Zola <zolamcadie@gmail.com>
2019-03-26 10:04:49 -0400

    fetch water levels for lake cowichan and return array

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
 
-/* GET home page. */
+const url = "https://wateroffice.ec.gc.ca/services/real_time_graph/json/inline?station=08HA009&start_date=2019-03-26&end_date=2019-03-26&param1=46"
+
 router.get('/', function(req, res, next) {
-  res.send("hydrometric data goes here");
+  axios.get(url)
+    .then(response => {
+      const levels = response.data['46'].provisional;
+      res.send(JSON.stringify(levels));
+    })
+    .catch(error => {
+      res.send(error);
+    });
 });
 
 module.exports = router;
