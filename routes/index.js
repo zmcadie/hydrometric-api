1aa1dbc49207c3fb000e05051097198a9ba050d6
Zola <zolamcadie@gmail.com>
2019-03-26 11:21:08 -0400

    Use closure to cache waterlevel data and only recheck every hour

--- routes/index.js
+++ routes/index.js
@@ -18,24 +18,37 @@ const getUrl = date => {
   return `https://wateroffice.ec.gc.ca/services/real_time_graph/json/inline?station=08HA009&start_date=${dateStr}&end_date=${dateStr}&param1=46`;
 }
 
-const fetchWaterLevel = cb => {
-  const now = new Date();
-  const pst = new Date(getPST(now));
-  const url = getUrl(pst);
-  axios.get(url)
-    .then(response => {
-      const levels = response.data['46'].provisional;
-      const latest = levels[levels.length - 1];
-      const latestObj = {
-        time: latest[0],
-        level: latest[1]
-      };
-      cb(200, latestObj);
-    })
-    .catch(error => {
-      cb(400, { error: error });
-    });
+const fetchWaterLevelClosure = () => {
+  let date = new Date();
+  let waterLevel = {};
+  return cb => {
+    const now = new Date();
+    if (date.getTime() - now.getTime() < 3600000 && Object.keys(waterLevel).length > 0) {
+      cb(200, waterLevel)
+    } else {
+      date = now;
+      const pst = new Date(getPST(date));
+      const url = getUrl(pst);
+      axios.get(url)
+        .then(response => {
+          const levels = response.data['46'].provisional;
+          const latest = levels[levels.length - 1];
+          const latestObj = {
+            time: latest[0],
+            level: latest[1]
+          };
+          waterLevel = latestObj;
+          cb(200, waterLevel);
+        })
+        .catch(error => {
+          console.error(error);
+          cb(400, {});
+        });
+    };
+  }
 }
+
+const fetchWaterLevel = fetchWaterLevelClosure();
 
 router.get('/', function(req, res, next) {
   const cb = (status, response) => res.status(status).json(response);
